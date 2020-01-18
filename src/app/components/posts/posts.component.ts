f25b4a1171d8b4cd5838b60fedba1dc2b1faaab0
AbdAllah El Gamal <abdallah.algamal93@gmail.com>
2020-01-19 01:37:57 +0530

    commit

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { Post } from  '../../interfaces/post';
+import {PostsService} from '../../services/posts/posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  public posts: Array<Post>;
+
+  constructor(private router: Router, private postsService: PostsService) { }
 
   ngOnInit() {
+    // if(!window.localStorage.getItem('token')) {
+    //   this.router.navigate(['login']);
+    //   return;
+    // }
+    this.postsService.getPosts().subscribe( (list: Array<Post>) => {
+          this.posts = list;
+          console.log(this.posts);
+      });
   }
+
+  deletePost(post: Post): void {
+    this.postsService.deletePost(post.id).subscribe( data => {
+        this.posts = this.posts.filter(u => u !== post);
+      })
+  };
+
+  editPost(post: Post): void {
+    window.localStorage.removeItem("editPostId");
+    window.localStorage.setItem("editPostId", post.id.toString());
+    this.router.navigate(['edit-post']);
+  };
+
+  addPost(): void {
+    this.router.navigate(['add-post']);
+  };
 
 }
---
+++ src/app/services/posts/posts.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from  '../../interfaces/post';
+import { Observable } from 'rxjs/index';
+import {ApiResponse} from  '../../interfaces/api.response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private http: HttpClient) { }
+  baseUrl: string = 'https://jsonplaceholder.typicode.com/posts';
+
+  getPosts(): Observable<Array<Post>> {
+    return this.http.get<Array<Post>>(this.baseUrl);
+  }
+
+  getPostById(id: number): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(this.baseUrl + id);
+  }
+
+  createPost(post: Post): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(this.baseUrl, post);
+  }
+
+  updatePost(post: Post): Observable<ApiResponse> {
+    return this.http.put<ApiResponse>(this.baseUrl + post.id, post);
+  }
+
+  deletePost(id: number): Observable<ApiResponse> {
+    return this.http.delete<ApiResponse>(this.baseUrl + '/' + id);
+  }
+}
